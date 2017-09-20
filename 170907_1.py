d1f27c7f26c8db92c99228c1f98bb82bc42b1627
undergraduate <cse20111636@cspro9.sogang.ac.kr>
2017-09-20 03:39:37 -0700

    add web crawling logic

--- 170907_1.py
+++ 170907_1.py
@@ -1,6 +1,30 @@
-f = open("data.txt", "r")
-r = f.read()
-words = r.split()
+import datetime, time
+import urllib.request
+import re
+def striphtml(data):
+    p = re.compile(r'<.*?>')
+    return p.sub('', data)
+
+url = "http://www.khaiyang.com/3175"
+
+f = urllib.request.urlopen(url)
+data = f.read().decode('utf-8')
+
+begin = data.find("<!-- 본문상단 우측 -->")
+end = data.find("지켜봐야 겠습니다")
+
+end += len("지켜봐야 겠습니다")
+
+
+print("total=", len(data))
+print("begin=", begin)
+print("length=", end-begin)
+
+review = data[begin:end]
+review = striphtml(review)
+review = re.sub(r'(\(.*\);|&nbsp|\▲|;)', '', review)
+review = re.sub(r'.*=.*', '', review)
+words = review.split()
 
 analyze = {}
 for word in words:
@@ -11,5 +35,5 @@ print("number of words is ", len(flist))
 cnt = 0
 for k, v in flist:
     print(k, v)
-    if cnt > 100: break
+    if cnt > 300: break
     cnt += 1
